24f170dfaa98d8b63be7e98bfee2ae26c48223bf
tipakA <emka456@onet.pl>
2022-10-04 03:44:25 +0200

    Initial commit

---
+++ src/bin/deploy.rs
@@ -0,0 +1,75 @@
+use std::env;
+use twilight_http::client::ClientBuilder;
+use twilight_model::application::command::{
+  BaseCommandOptionData, ChoiceCommandOptionData, CommandOption, CommandType,
+  OptionsCommandOptionData,
+};
+use twilight_util::builder::command::CommandBuilder;
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+  dotenvy::dotenv()?;
+  let roles_command = CommandBuilder::new("roles", "Select roles you want", CommandType::ChatInput)
+    .dm_permission(false)
+    .build();
+
+  let config_command = CommandBuilder::new(
+    "config",
+    "Manage selfroles for the server",
+    CommandType::ChatInput,
+  )
+  .dm_permission(false)
+  .option(CommandOption::SubCommand(OptionsCommandOptionData {
+    name: "add".to_string(),
+    description: "Add new selfrole".to_string(),
+    options: vec![
+      CommandOption::Role(BaseCommandOptionData {
+        name: "role".to_string(),
+        description: "Select a role".to_string(),
+        required: true,
+        ..Default::default()
+      }),
+      CommandOption::String(ChoiceCommandOptionData {
+        name: "label".to_string(),
+        description: "Role name that will be displayed in the select menu".to_string(),
+        required: true,
+        ..Default::default()
+      }),
+      CommandOption::String(ChoiceCommandOptionData {
+        name: "description".to_string(),
+        description: "Optional description displayed in the select menu".to_string(),
+        ..Default::default()
+      }),
+    ],
+    ..Default::default()
+  }))
+  .option(CommandOption::SubCommand(OptionsCommandOptionData {
+    name: "remove".to_string(),
+    description: "Remove a selfrole".to_string(),
+    options: vec![
+      CommandOption::Role(BaseCommandOptionData {
+        name: "role".to_string(),
+        description: "Select a role".to_string(),
+        required: true,
+        ..Default::default()
+      }),
+    ],
+    ..Default::default()
+  }))
+  .build();
+
+  let client = ClientBuilder::new()
+    .token(env::var("TOKEN")?)
+    .build();
+  
+  let app_id = client.current_user_application()
+    .exec()
+    .await?
+    .model()
+    .await?
+    .id;
+
+  client.interaction(app_id).set_global_commands(&[roles_command, config_command]).exec().await?;
+
+  Ok(())
+}
