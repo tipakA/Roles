5abfaa80439d8ab9ae620767871fdae80a5ea595
tipakA <emka456@onet.pl>
2022-10-08 23:03:31 +0200

    Build i18n data file

--- build.rs
+++ build.rs
@@ -1,8 +1,35 @@
-use std::path::Path;
+use icu::locid::{langid, LanguageIdentifier};
+use icu_datagen::{CldrLocaleSubset, Out, SourceData};
+use std::{fs::File, path::Path};
 
-fn main() {
+fn new_database() {
   let exists = Path::new("db.db").try_exists();
   if !matches!(exists, Ok(true)) {
     let _ = std::fs::copy("schema.db", "db.db");
   }
+}
+
+const DISCORD_LOCALES: &[LanguageIdentifier] = &[langid!("en-GB"), langid!("en"), langid!("pl")];
+
+fn icu_gen_list() {
+  icu_datagen::datagen(
+    Some(DISCORD_LOCALES),
+    &icu_datagen::keys(&[
+      "list/and@1",
+      "fallback/likelysubtags@1",
+      "fallback/parents@1",
+    ]),
+    &SourceData::default()
+      .with_cldr_for_tag("41.0.0", CldrLocaleSubset::Modern)
+      .unwrap(),
+    vec![Out::Blob(Box::new(
+      File::create("i18n/data.postcard").unwrap(),
+    ))],
+  )
+  .unwrap();
+}
+
+fn main() {
+  new_database();
+  icu_gen_list();
 }
